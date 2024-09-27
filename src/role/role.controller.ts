c5b90366159e172c6e21856fa52e26f85354ad05
Juninho619 <christophebontemps619@gmail.com>
2024-09-27 16:16:48 +0200

    role controller

--- src/role/role.controller.ts
+++ src/role/role.controller.ts
@@ -1,7 +1,63 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+  //   UseGuards,
+} from '@nestjs/common';
+import { GetUser } from '../auth/get-user.decorator'
+// import { JwtGuard } from '../auth/guard';
+import { RoleDto } from './dto';
 import { RoleService } from './role.service';
+import { ApiTags } from '@nestjs/swagger';
+import { User } from '@prisma/client';
+import { JwtGuard } from 'src/auth/guard';
 
+@UseGuards(JwtGuard)
+@ApiTags('role')
 @Controller('role')
 export class RoleController {
-  constructor(private readonly roleService: RoleService) {}
+  constructor(private roleService: RoleService) {}
+
+  @Get('/all')
+  getAllRoles(@GetUser() user: User) {
+    return this.roleService.getRolesAdmin(user.id);
+  }
+  // @Get('/cm/all')
+  // getRolesCm(@GetUser() user: User) {
+  //     return this.roleService.getRolesCm(user)
+  // }
+
+  @HttpCode(HttpStatus.CREATED)
+  @Post('/create')
+  createRole(
+    // @GetUser() user: User,
+    @Body()
+    dto: RoleDto,
+    @GetUser() user: User,
+  ) {
+    return this.roleService.createRole(user.id, dto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Patch('/update/:id')
+  editRoleById(
+    @Param('id') roleId: string,
+    @Body() dto: RoleDto,
+    @GetUser() user: User,
+  ) {
+    return this.roleService.editRoleById(user.id, roleId, dto);
+  }
+
+  @HttpCode(HttpStatus.NO_CONTENT)
+  @Delete('/delete/:id')
+  deleteRoleById(@Param('id') roleId: string, @GetUser() user: User) {
+    return this.roleService.deleteRoleById(user.id, roleId);
+  }
 }
